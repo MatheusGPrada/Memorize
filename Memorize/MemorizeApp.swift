76935369049dca9e774b67e7dda809da86d1e2e8
MatheusGPrada <matheus.prada@outlook.com>
2021-10-28 12:25:53 -0300

    Iniciando o projeto

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Matheus Prada on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
